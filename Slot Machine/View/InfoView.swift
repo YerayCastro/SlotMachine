910be1841827b51df3d425b09a44059d69f48947
YerayCastro <112658475+YerayCastro@users.noreply.github.com>
2023-06-12 14:51:46 +0200

    commit

---
+++ Slot Machine/View/InfoView.swift
@@ -0,0 +1,72 @@
+//
+//  InfoView.swift
+//  Slot Machine
+//
+//  Created by Yery Castro on 5/6/23.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body: some View {
+        VStack(alignment: .center, spacing: 10) {
+            LogoView()
+            
+            Spacer()
+            
+            Form {
+                Section(header: Text("About the application")) {
+                    FormRowView(firstItem: "Application", secondItem: "Slot Machine")
+                    FormRowView(firstItem: "Plataforms", secondItem: "iPhone, iPad, Mac")
+                    FormRowView(firstItem: "Developer", secondItem: "Jonh / Jane")
+                    FormRowView(firstItem: "Designer", secondItem: "Robert Petras")
+                    FormRowView(firstItem: "Music", secondItem: "Dan Lebowitz")
+                    FormRowView(firstItem: "Website", secondItem: "swuiftuimasterclass.com")
+                    FormRowView(firstItem: "Copyright", secondItem: " 2020 All rights reserverd.")
+                    FormRowView(firstItem: "Version", secondItem: "1.0.0")
+                }
+            }
+            .font(.system(.body, design: .rounded))
+        }
+        .padding(.top, 40)
+        .overlay(
+            Button(action: {
+                audioPlayer?.stop()
+                self.presentationMode.wrappedValue.dismiss()
+            }) {
+                Image(systemName: "xmark.circle")
+                    .font(.title)
+            }
+                .padding(.top, 30)
+                .padding(.trailing, 20)
+                .accentColor(Color.secondary)
+            , alignment: .topTrailing
+        )
+        .onAppear(perform: {
+            playSound(sound: "background-music", type: "mp3")
+        })
+    }
+}
+
+struct FormRowView: View {
+    var firstItem: String
+    var secondItem: String
+    
+    var body: some View {
+        HStack {
+            Text(firstItem).foregroundColor(Color.gray)
+            Spacer()
+            Text(secondItem)
+        }
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView()
+    }
+}
+
+
